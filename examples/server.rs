b4e774715a8c139f71d1a6f07e3024cb2da75fec
altacountbabi <radustefan379@gmail.com>
2024-06-08 14:49:51 +0300

    Added reply function for server

--- examples/server.rs
+++ examples/server.rs
@@ -1,26 +1,12 @@
 use linux_ipc::IpcChannel;
-use serde::{Deserialize, Serialize};
-
-#[derive(Debug, Serialize, Deserialize)]
-#[allow(dead_code)]
-struct Post {
-    title: String,
-    body: String,
-}
 
 fn main() {
-    println!("Creating example channel");
     let mut channel = IpcChannel::new("/tmp/example.sock").expect("Failed to create channel");
 
     loop {
-        println!("Receiving post in channel `init`");
-        let result = channel.receive::<Post>();
+        println!("Listening for messages...");
+        let (result, reply) = channel.receive::<String, String>().expect("Failed to receive post");
 
-        if result.is_err() {
-            eprintln!("{}", result.unwrap_err());
-        } else {
-            let result = result.unwrap();
-            println!("{:?}", result);
-        }
+        reply(result).expect("Failed to reply to client");
     }
 }
