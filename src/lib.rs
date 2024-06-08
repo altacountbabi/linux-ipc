f670e1b6d10c4a75d6d2b7c65932ddd20129dbca
altacountbabi <altacountbabi@users.noreply.github.com>
2024-06-08 14:49:51 +0300

    Added reply function for server

--- src/lib.rs
+++ src/lib.rs
@@ -15,6 +15,8 @@ pub struct IpcChannel {
     is_client: bool,
 }
 
+type ReplyFn<T> = Box<dyn FnOnce(T) -> io::Result<()>>;
+
 impl IpcChannel {
     pub fn new(path: &str) -> io::Result<Self> {
         fs::remove_file(path).ok();
@@ -44,15 +46,14 @@ impl IpcChannel {
             let stream = UnixStream::connect(&self.socket_path)?;
             self.stream = Some(stream);
         }
-        self.stream.as_mut().ok_or_else(|| {
-            io::Error::new(io::ErrorKind::NotConnected, "Failed to connect to stream")
-        })
+        self.stream
+            .as_mut()
+            .ok_or_else(|| io::Error::new(io::ErrorKind::NotConnected, "Failed to connect to stream"))
     }
 
     pub fn send<T: Serialize>(&mut self, value: T) -> io::Result<Option<String>> {
         let stream = self.get_stream()?;
-        let binary =
-            bincode::serialize(&value).map_err(|e| io::Error::new(io::ErrorKind::Other, e))?;
+        let binary = bincode::serialize(&value).map_err(|e| io::Error::new(io::ErrorKind::Other, e))?;
 
         stream.write_all(&binary)?;
         stream.flush()?;
@@ -61,8 +62,6 @@ impl IpcChannel {
         let mut response = String::new();
         stream.read_to_string(&mut response)?;
 
-        self.stream = None;
-
         if response.is_empty() {
             Ok(None)
         } else {
@@ -70,18 +69,27 @@ impl IpcChannel {
         }
     }
 
-    pub fn receive<T: DeserializeOwned + Debug>(&mut self) -> io::Result<T> {
+    pub fn receive<T: DeserializeOwned + Debug, R: Serialize>(&mut self) -> io::Result<(T, ReplyFn<R>)> {
         let listener = self
             .listener
             .as_mut()
             .ok_or_else(|| io::Error::new(io::ErrorKind::NotConnected, "Listener not found"))?;
-        let mut stream = listener.accept()?.0;
+        let (mut stream, _) = listener.accept()?;
 
         let mut buffer = Vec::new();
         stream.read_to_end(&mut buffer)?;
 
-        bincode::deserialize::<T>(&buffer)
-            .map_err(|err| io::Error::new(io::ErrorKind::InvalidData, err))
+        let request: T = bincode::deserialize(&buffer).map_err(|err| io::Error::new(io::ErrorKind::InvalidData, err))?;
+
+        let reply_fn = move |value: R| {
+            let binary = bincode::serialize(&value).map_err(|e| io::Error::new(io::ErrorKind::Other, e))?;
+
+            stream.write_all(&binary)?;
+            stream.flush()?;
+            Ok(())
+        };
+
+        Ok((request, Box::new(reply_fn)))
     }
 }
 
